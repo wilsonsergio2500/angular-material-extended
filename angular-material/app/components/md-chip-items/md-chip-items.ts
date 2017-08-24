3a74b541aacdd943e62c60dae36de696141770b4
Sergio_Wilson <Sergio_Wilson@ORFWILSS5550WX>
2017-08-24 18:03:55 -0500

    md-chip abastraction

---
+++ angular-material/app/components/md-chip-items/md-chip-items.ts
@@ -0,0 +1,36 @@
+﻿
+import * as angular from 'angular';
+import { APP_MODULE } from '../../main/index';
+
+namespace Component.mdChipItems{
+
+
+    export class MdChipItemCtrl {
+        mdPlaceholder: string;
+        mdQuery: (searchText: string) => angular.IPromise<any[]> | any[] ;
+        mdFieldName: string;
+        ngModel: string;
+        constructor() {
+        }
+    }
+
+    const template = require('!!raw-loader!./md-chip-items.html');
+    function mdChipItems(){
+        return <angular.IDirective>{
+            bindToController: true,
+            controller: MdChipItemCtrl,
+            template: template,
+            controllerAs: 'vm',
+            require: 'ngModel',
+            scope: {
+                ngModel: '=',
+                mdPlaceholder: '@',
+                mdFieldName:'@',
+                mdQuery: '='
+            }
+        }
+    }
+
+    APP_MODULE.directive('mdChipItems', mdChipItems);
+
+}
